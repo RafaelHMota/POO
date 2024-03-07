b6b768721ad478e26ad1956a7a53ae19e1d9ee74
Rafael <144626992+RafaelHMota@users.noreply.github.com>
2024-03-07 20:16:14 -0300

    Add files via upload

---
+++ poo_01/src/Aluno.java
@@ -0,0 +1,17 @@
+public class Aluno  {
+    private int matricula;
+
+    private String nome;
+    private String cpf;
+    private String dataNascimeno;
+    private String endereco;
+    private float peso;
+    private String telefone;
+    private String email;
+    private String genero;
+    private Boolean atestado;
+    private Double alura;
+}
+
+
+
---
+++ poo_01/src/Equipamentos.java
@@ -0,0 +1,2 @@
+public class Equipamentos {
+}
